6e51be4cb2c614b96b153e20e9cffb6826a5783e
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-04-11 20:22:04 -0300

    atualização de exercicios

---
+++ exercicios/ex.04_sala.pas
@@ -0,0 +1,8 @@
+Program Boletim;
+  Var Nota1, Nota2, Nota3,Media: real;
+Begin      
+  Write('Digite 3 notas do aluno');
+  Readln (Nota1, Nota2, Nota3);
+	Media:=(Nota1 + Nota2 + Nota3)/3;
+	Writeln (Media);  
+End.
