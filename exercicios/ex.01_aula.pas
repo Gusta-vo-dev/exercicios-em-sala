cede1f396b93ad5960657e3368e8ad8443f4bd01
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-04-11 20:24:48 -0300

    atualização de exercicios

---
+++ exercicios/ex.01_aula.pas
@@ -0,0 +1,9 @@
+Program Area_quadrado;
+  VAR Lado, Area:REAL;
+
+Begin
+    Writeln ('Informe o lado do quadrado');
+    Readln (Lado);
+    Area:= Lado*Lado;
+    Writeln (Area:6:2);
+end.  
---
+++ exercicios/ex.02_aula.pas
@@ -0,0 +1,9 @@
+Program Perimetro_quadrado;
+  Var Lado, Perimetro:Real;
+
+Begin
+    Writeln ('Informe o lado do quadrado');
+    Readln (Lado);
+    Perimetro:= 4*Lado;
+    Writeln (Perimetro:6:2);
+end.  
