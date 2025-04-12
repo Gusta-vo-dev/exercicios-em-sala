18c820d6f6d6f184cbd923e95e425ab7cd99cbc3
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-04-11 21:27:41 -0300

    update

---
+++ exercicios/ex.12_aula.pas
@@ -0,0 +1,13 @@
+Program ex_12 ;
+
+		var a, b,c:integer;
+
+Begin
+   writeln ('Digite um valor para A e um para B');
+   readln (a, b);
+   if a = b then
+   		c:= a + b
+   else 
+   	c:= a * b;
+   writeln ('O resultado é: ', C); 
+End.
