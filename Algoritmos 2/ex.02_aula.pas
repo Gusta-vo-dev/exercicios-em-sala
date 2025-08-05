494f17c731548170267213ec7d454139e28ef7f2
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-08-05 20:55:14 -0300

    update exercise

---
+++ Algoritmos 2/ex.02_aula.pas
@@ -0,0 +1,24 @@
+Program ex_02 ; 
+
+	const cont=200;
+	var i, valor: integer;
+			num: array[1..200] of integer;
+			v: array[1..25] of integer;  
+		 
+Begin
+    for i:=1 to cont do 
+    	begin
+    		randomize; 
+				num[i]:= 1 + random (25);
+    		writeln (num[i], ' ');
+			end;
+		for i:=1 to cont do
+			begin 
+				valor:= num[i];
+				v[valor]:= v[valor] + 1;
+			end;
+		for i:=1 to 25 do 
+			begin
+				writeln (' Número ', i, ' são ', v[i]);  
+			end;  
+End.
