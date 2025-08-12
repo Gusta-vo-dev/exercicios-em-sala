ea71194f5fa046de134284ce80e238c5e9ed2878
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-08-11 21:11:38 -0300

    nova pasta de aprendizagem(HTML)

--- Fundamentos-SI/aula9-pratica.3.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO TBCLIENTE
- (CLICODIGO, CLINOME, CLIENDERECO, CLITELEFONE)
-VALUES
- (1, 'Cleber Nardelli', 'Rua XV de Novembro, 25', '(47)98822-9988'),
- (2, 'Alice Nardelli', 'Rua XV de Novembro, 25', '(47)98822-9988')
