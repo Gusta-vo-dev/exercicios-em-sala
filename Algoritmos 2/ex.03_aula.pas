d3fc83bf82ca8d9352c0eb539674fc78dfb40d38
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-08-12 21:37:38 -0300

    up

--- Algoritmos 2/ex.03_aula.pas
+++ Algoritmos 2/ex.03_aula.pas
@@ -3,5 +3,20 @@ Program ex_03;
 		var       
 
 Begin
-  	
+		 for 
+  	 begin
+  	 	if casa = times + 1 then // Controla para não ultrapassar a quantidade de times
+  	 			casa:=  2;
+  	 	if visitante = 1 then //Controla para que o visitante não seje 1
+  	 			visitante = times;
+  	 	if i mod 2 = 1 then 
+  	 			writeln ( casa:2, ' x ', visitante:2)
+  	 	else 
+  	 			writeln (visitante:2, ' x ', casa:2);
+  	 	if casa = 1 then // Gera o time da casa do segundo jogo da rodada 
+  	 			casa:= visitante + 1;
+  	 	else 
+			 		casa:= casa + 1;
+			visitante := visitante - 1;
+			end;		
 End.
