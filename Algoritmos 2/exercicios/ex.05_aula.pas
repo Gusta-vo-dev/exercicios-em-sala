292ecb2d1ac56f2a7efe91413ed4ba644d876413
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-09-02 21:42:49 -0300

    update

--- Algoritmos 2/exercicios/ex.05_aula.pas
+++ Algoritmos 2/exercicios/ex.05_aula.pas
@@ -1,18 +1,29 @@
 Program ex_05 ;
 
-		var i, qntd: integer;
-				n: array[1..10] of integer;
-
+		var i, num, j, qntd:integer;
+				a: array[1..10] of integer;
+				b: array[1..10] of integer;
 Begin
+	 //digita qntd
 	 writeln ('Digite a quantidade de elementos do vetor ( MÁX = 10 )');
 	 readln ( qntd );
-	 i:= 1;
-   while i<= 10 do
-   begin                                              
-   		writeln ('Ditige o ' ,i, 'º número do vetor!');          
-   		readln (  n[i] );
-   		i:= i + 1;
-   end;
-   for i:=1 to qntd do
-   	   writeln ( n[i] );
+	 for i:=1 to qntd do
+	 begin
+	 		writeln;
+	 		//digita numeros do vetor A
+	 		writeln ('Digite o ', i, 'º número do vetor');
+	 		readln ( num );
+	 		a[i]:= num;
+				 //verifica se elemento está no vetor
+				 for j:=1 to qntd do
+				 begin
+				 		if num = a[j] then
+				 			b[j]:= a[j];
+				 end;
+	end;	
+	for i:=1 to qntd do
+  begin
+  		writeln ('Números digitados: ', a[i] );
+			writeln ('Números repetidos: ', b[i] );
+	end;		 		
 End.
