17dbb7fcb2e330886b117a1dc3a05ea23a54fbb4
Gusta-vo-dev <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-09-12 22:22:08 -0300

    up

--- Algoritmos 2/ex.05_aula.pas
+++ Algoritmos 2/ex.05_aula.pas
@@ -1,18 +1,26 @@
 Program ex_05 ;
 
-		var i, qntd: integer;
+		var i, j, qntd: integer;
 				n: array[1..10] of integer;
+				achou: boolean;
 
 Begin
 	 writeln ('Digite a quantidade de elementos do vetor ( MÁX = 10 )');
 	 readln ( qntd );
-	 i:= 1;
-   while i<= 10 do
+	 i:= 1; 
+	 achou:= false;     
+   while i<= qntd do
    begin                                              
    		writeln ('Ditige o ' ,i, 'º número do vetor!');          
    		readln (  n[i] );
+   		j:=2;
+   		while j<= qntd do3
+   		begin 
+   				if n[i] = n[j-1] then
+   						writeln ('Número repetido, digiti-o novamente!');
+   						achou:= true;
+   						break;
+   		end;
    		i:= i + 1;
    end;
-   for i:=1 to qntd do
-   	   writeln ( n[i] );
 End.
