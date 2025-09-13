ed70b14945fa69e39f2140719526ae8348c6d838
Gusta-vo-dev <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-09-12 22:28:06 -0300

    pull

--- Algoritmos 2/ex.05_aula.pas
+++ Algoritmos 2/ex.05_aula.pas
@@ -1,5 +1,6 @@
-Program ex_05 ;
+Program ex_05;
 
+<<<<<<< HEAD
 		var i, j, qntd: integer;
 				n: array[1..10] of integer;
 				achou: boolean;
@@ -23,4 +24,32 @@ Begin
    		end;
    		i:= i + 1;
    end;
+=======
+		var i, j, n, repetiu: integer;
+		    v: array[1..10] of integer;
+
+Begin
+    i:=1;
+		while i<=10 do
+			begin 
+					writeln ('Digite um número inteiro');
+					readln( n );
+					for j:=1 to i - 1 do
+						begin
+							 if v[j] = n then 
+							 		repetiu:= 1;
+						end;
+					if repetiu = 1 then
+						begin
+								writeln ('Valor repetido, digite outro');	
+						end
+					else 
+						begin
+								v[i]:= n;
+						end;
+				 i:= i + 1;			
+		  end;
+		for i:=1 to n do 
+			writeln (v[i], ' ');		
+>>>>>>> 7f902a6764fb52be226a22989b45c36abe80f7fa
 End.
