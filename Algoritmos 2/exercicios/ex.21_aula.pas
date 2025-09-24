9ebbedece334db7cbf64160f55a0eceac45ce951
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-09-23 21:36:56 -0300

    up

--- Algoritmos 2/exercicios/ex.21_aula.pas
+++ Algoritmos 2/exercicios/ex.21_aula.pas
@@ -1,6 +1,6 @@
 Program ex_21;
 
-		var l,c, j, maiorl, maiorc: integer;
+		var l,c, j, maior: integer;
 				a: array[1..3, 1..3] of integer;
 				ml, mc: array[1..3] of integer;
 
@@ -9,9 +9,9 @@ Begin
     	for c:=1 to 3 do 
     	begin
     			gotoxy(c*4, l+6);
-    			a[l,c]:= random(20) + 0;
+    			a[l,c]:= random(20) + 1;
     			write (a[l,c]);
     	end;
-    writeln;
+    writeln;     
     writeln;
 End.
