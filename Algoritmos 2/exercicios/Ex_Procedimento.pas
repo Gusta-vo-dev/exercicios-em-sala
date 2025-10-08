7c16fde4b4b5fbf506f6b7ce4ed3b1fe0849608b
Gustavo Pickler Vieira <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-10-07 21:42:45 -0300

    Criado ex_Procedimentos

---
+++ Algoritmos 2/exercicios/Ex_Procedimento.pas
@@ -0,0 +1,34 @@
+Program Exemplo_Procedimentos;
+
+type vetor = array[1..5] of integer;
+
+var a, b:vetor;
+		tam_a, tam_b: integer;
+
+//Procedimento para ler vetor
+procedure ler_vetor (var v:vetor; tam: integer);
+var i:integer;
+
+begin
+  for i:=1 to tam do
+  readln (v[i]);
+end;
+
+//Procedimento para escrever vetor
+procedure escreve_vetor (v:vetor; tam:integer);
+var i:integer;
+
+begin
+  for i:=1 to tam do
+  write (v[i], ' ');
+end;
+
+//Programa Principal
+Begin
+	writeln ('Informe o tamanho do vetor A e B');
+	readln (tam_a, tam_b);
+  ler_vetor(a,tam_a);
+  ler_vetor(b,tam_b);
+  escreve_vetor (a,tam_a);
+  escreve_vetor (b,tam_b);
+End.
