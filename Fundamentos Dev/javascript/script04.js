6f36a6cbb0a9a5b67ee299be2062b9389315ffea
Gusta-vo-dev <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-10-19 20:38:26 -0300

    update

---
+++ Fundamentos Dev/javascript/script04.js
@@ -0,0 +1,14 @@
+let nome = "Gustavo";
+let idade = 18;
+console.log("Meu nome é " + nome + " e eu tenho " + idade + " anos.");  
+
+function somar (5 3){
+    return 5 + 3;
+}
+
+let resultado = somar;
+console.log (resultado);
+somar (5, 3);
+
+
+//INSTALAR CODE RUN - EXTENSÃO
