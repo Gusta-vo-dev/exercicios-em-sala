1b37c0564be6fdf04eb5b6f35f88e274b72008d0
Gusta-vo-dev <114954844+Gusta-vo-dev@users.noreply.github.com>
2025-10-26 18:58:10 -0300

    treinando javascript

---
+++ Fundamentos Dev/javascript/tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+let nome = 'Gustavo';
+let idade = 18;
+let cidade = 'Petrolândia';
+console.log('Olá, meu nome é', nome, ' tenho ', idade, ' anos e moro em ', cidade);
+
+for (let i = 0; i < 10; i++){
+    console.log('Número: ', i);
+}
+
+for (let i = 0; i < 10; i++){
+   if ( i % 2 == 1 ){
+    console.log('O número ', i, ' é impar');
+   }
+} 
+
+let contador = 0;
+let fixo = 5;
+while (contador <= 10){
+    let multi = contador * 5;
+    console.log(fixo, ' x ', contador, ' = ', multi);
+    contador = contador + 1;
+}
