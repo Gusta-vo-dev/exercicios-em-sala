16b9b95eb85aad9f3c5e6b3bb4196bb93bf7d36f
Gusta-vo-dev <114954844+Gusta-vo-dev@users.noreply.github.com>
2026-04-01 22:38:46 -0300

    Atualizando o Projeto do Jogo de Truco em Pascal!

--- JogoTruco.pas
+++ JogoTruco.pas
@@ -5,7 +5,7 @@ Program JogoTruco ;
 			carta: integer;
 			naipe: string;
 			forca: integer;
-		end;
+	end;
 		iBaralho = array[1..40] of iCarta;
 		
 	
@@ -14,6 +14,8 @@ Program JogoTruco ;
 	    naipes: array[1..4] of string;
 	    cartas: array[1..10]of integer;
 	begin
+			writeln('			BARALHO INICIADO E EMBARALHADO. BORA PRO JOGO MARRECO!');
+			writeln;
 			naipes[1]:= 'Paus';
 			naipes[2]:= 'Copas';
 			naipes[3]:= 'Espadas';
@@ -28,7 +30,6 @@ Program JogoTruco ;
 				 				begin
 								 	  carta:=i; 								
 								 	  naipe:= naipes[n];
-								 	  forca:= x;
 								 	  x:= x + 1;
 								end;
 						end;
@@ -50,18 +51,82 @@ Program JogoTruco ;
 	  end;
 	end;
 	
-	var baralho: iBaralho;
-			cont: integer;
+	procedure corteBaralho ( var baral: iBaralho; var corte: integer ) ;
+	var i, x: integer;
+			novoBaralho: iBaralho;
+	begin
+			x:= 1;
+			write ('			Digite em qual posição você quer cortar o baralho(1 à 40): ');
+			readln ( corte );
+			for i:=corte to 40 do
+			begin
+					novoBaralho[x]:= baral[i];
+					x:= x + 1;
+			end; 
+			for i:=1 to (corte - 1) do 
+			begin
+					novoBaralho[x]:= baral[i];
+					x:= x + 1;
+			end;
+			baral:= novoBaralho;
+	end;
+	
+	//procedure inserirForca ( baral: iBaralho; var forc: iBaralho; jok: iCarta);
+	//var  i: integer;
+	//begin
+	   
+	//end
+	
+	procedure darCartas ( var baral: iBaralho; var maoJ, maoC: iBaralho; var jok: iCarta);
+	var i, posiJ, posiC, corte:integer;
+	begin
+		 posiJ:= 1;
+		 posiC:= 1;
+		 corteBaralho ( baral, corte );
+		 		 for i:=1 to 6 do
+		 		 begin
+		     		if ( corte mod 2 = 1) then
+		     		begin
+		    				maoJ[posiJ]:= baral[i];
+		    				posiJ:= posiJ + 1;
+		    		end
+		    		else 
+		    		begin
+		    				maoC[posiC]:= baral[i];
+		    				posiC:= posiC + 1;
+		    		end;
+		    		corte:= corte + 1;
+		     end;
+		     jok:= baral[7];
+		     writeln;
+		     writeln('			Sua mão ficou assim: ');
+		     writeln('			---------------------------');
+		     write('			');
+		     for i:=1 to 3 do 
+		     begin
+		     		write (maoJ[i].carta, ' de ', maoJ[i].naipe, '  |  ');
+		     end;
+		     writeln;
+		     writeln;
+		     writeln;
+				 writeln ('			A que vira pra coringa é: ', jok.carta, ' de ', jok.naipe);
+				 //inserirForca ( baral, forc, jok);
+	end; 
+	
+	
+	
+	var  //Variável para o Baralho Geral//
+			baralho: iBaralho;
+		
+	var  //Variáveis para dar cartas//
+			maoJogador: iBaralho;
+			maoComputador: iBaralho;
+			joker: iCarta;
+			
 	
 Begin
   inserirBaralho(  baralho );
   embaralhar(baralho);
-  for cont:=1 to 40 do
-  begin
-  	with baralho[cont] do
-  	begin
-			writeln ( ' ', carta,' de ', naipe);
-		end;
-  end;
+  darCartas ( baralho, maoJogador, maoComputador, joker);
   readln;
 End.
